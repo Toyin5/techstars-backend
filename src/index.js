c15e6f22742d8bf19f4931ace6d99525ac05cfb3
Toyin Muhammed <toyinmuhammed50@gmail.com>
2023-09-05 23:12:09 +0200

    vercel

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import database from "./utils/db.js";
 import "dotenv/config";
 import cors from "cors";
 import userRoute from "./routes/user.js";
+import chatRoutes from "./routes/chat.js";
 
 const app = express();
 const PORT = process.env.PORT || 8080;
@@ -11,6 +12,7 @@ app.use(cors());
 app.use(express.static("/public"));
 app.use(express.json());
 app.use("/api", userRoute);
+app.use("/api", chatRoutes);
 
 await database();
 
